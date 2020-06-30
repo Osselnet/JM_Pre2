bbf5c45784fc59dffbcec62b3b01b6fe8d898aea
Oleg Sergienko <hornets@list.ru>
2020-06-30 23:25:32 +0300

    Создание первоначальной заготовки

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,69 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import web.model.User;
+import web.service.UserService;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    protected UserService userService;
+
+    @GetMapping("/signup")
+    public String showSignUpForm(User user) {
+        return "add-user";
+    }
+
+    @PostMapping("/adduser")
+    public String addUser(User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+
+        //userRepository.save(user);
+        model.addAttribute("users", userService.getAllUser());
+        return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userService.getUserById(id);
+        //.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, User user,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "update-user";
+        }
+
+        //userRepository.save(user);
+        model.addAttribute("users", userService.getAllUser());
+        return "redirect:/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        //User user = userService.getUserById(id);
+        //.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userService.deleteUser(id);
+        model.addAttribute("users", userService.getAllUser());
+        return "index";
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,51 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private long age;
+
+    public User() {
+    }
+
+    public User(Long id, String name, long age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+}
